5b820a5ea20ff71ac5181f854a0a1884b84cdc99
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-07-22 22:40:16 +0300

    Fix the bash script for installiation

--- setup.sh
+++ setup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# Set Color
+RED="\e[31m"
+GREEN="\e[32m"
+BLUE="\e[34m"
+ENDCOLOR="\e[0m"
+
 # For root control
 if [ "$(id -u)" != 0 ]; then
-  echo "You are not root! This script must be run as root"
+  printf "${RED}"
+  cat <<EOL
+========================================================================
+You are not root! This script must be run as root!
+========================================================================
+EOL
+  printf "${ENDCOLOR}"
   exit 1
 fi
 
@@ -14,18 +26,13 @@ writeInstallationSuccessfulMessage() {
   printf "${GREEN}========================$1 is installed successfully!========================${ENDCOLOR}\n"
 }
 
-# Set Color
-RED="\e[31m"
-GREEN="\e[32m"
-BLUE="\e[34m"
-ENDCOLOR="\e[0m"
-
 # Set Version
 JETBRAINS_VERSION=2022.1.3
 GO_VERSION=1.18.3
 POSTMAN_VERSION=9.20.3
 MAVEN=3
 MAVEN_VERSION=3.8.6
+GRADLE_VERSION=7.5
 DROIDCAM_VERSION=1.8.1
 DROPBOX_VERSION=2020.03.04
 
@@ -58,7 +65,8 @@ dnf install -y \
   tree \
   zsh \
   dialog \
-  gnome-font-viewer
+  gnome-font-viewer \
+  Htop
 printf "\n${BLUE}===============Standard packages are installed successfully===============${ENDCOLOR}\n"
 
 cmd=(dialog --title "Fedora 35 Installer" --separate-output --checklist 'Please choose: ' 27 76 16)
@@ -73,7 +81,7 @@ options=(
   B5 "Microsoft Edge" off
   # C: Chat Application
   C1 "Zoom Meeting Client" off
-  C2 "Discord" off
+  C2 "Discord (Snap)" off
   C3 "Thunderbird Mail" off
   C4 "Skype (Snap)" off
   C5 "Slack (Snap)" off
@@ -88,24 +96,19 @@ options=(
   D7 "Postman" off
   D8 "Docker" off
   D9 "Maven" off
-  D10 "Putty" off
-  D11 "Vim" off
-  D12 "PyCharm" off
-  D13 "Robo 3T" off
-  D14 "DataGrid" off
-  D15 "Mongo Shell & MongoDB Database Tools" off
+  D10 "Gradle" off
+  D11 "Putty" off
+  D12 "Vim" off
+  D13 "DataGrid" off
+  D14 "Mongo Shell & MongoDB Database Tools" off
   # E: Gnome Tweaks
   E1 "Gnome Tweak Tool" off
   E2 "Gnome Shell Extensions" off
   # F: Utility
   F1 "Dropbox" off
   F2 "KeePassXC" off
-  F3 "Virtualbox" off
-  F4 "Terminator" off
-  F5 "Powerline" off
-  F6 "Htop" off
-  F7 "vimrc" off
-  F8 "Ulauncher" off
+  F3 "Terminator" off
+  F4 "Gnome Boxes" off
   # G: Image, Video and Audio
   G1 "GIMP" off
   G2 "Droidcam" off
@@ -153,8 +156,10 @@ for choice in $choices; do
     ;;
   B5)
     writeInstallationMessage Microsoft-Edge
-    wget -O edge.tar.gz https://packages.microsoft.com/yumrepos/edge/microsoft-edge-stable-103.0.1264.51-1.x86_64.rpm
-    dnf -y install microsoft-edge-stable-103.0.1264.51-1.x86_64.rpm
+    rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
+    dnf update --refresh
+    dnf install microsoft-edge-stable
     writeInstallationSuccessfulMessage Microsoft-Edge
     ;;
 
@@ -199,36 +204,25 @@ for choice in $choices; do
     writeInstallationMessage JAVA-JDK-18
     wget https://download.oracle.com/java/18/latest/jdk-18_linux-x64_bin.tar.gz
     tar xf jdk-18_linux-x64_bin.tar.gz -C /usr/local/java/
-
     update-alternatives --install "/usr/bin/java" "java" "/usr/local/java/jdk-18.0.1.1/bin/java" 1
     update-alternatives --install "/usr/bin/javac" "javac" "/usr/local/java/jdk-18.0.1.1/bin/javac" 1
-
     update-alternatives --set java /usr/local/java/jdk-18.0.1.1/bin/java
     update-alternatives --set javac /usr/local/java/jdk-18.0.1.1/bin/javac
-
-    touch /etc/profile.d/javaenv.sh
-    echo ' ' >>/etc/profile.d/javaenv.sh
-    echo 'export JAVA_HOME=/opt/jdk-18.0.1.1' >>/etc/profile.d/javaenv.sh
-    echo 'export PATH=$PATH:$HOME/bin:$JAVA_HOME/bin' >> /etc/profile.d/javaenv.sh
+    echo -e ' \n"# Java Configuration\nexport JAVA_HOME=/opt/jdk-18.0.1.1\nexport PATH=$PATH:$HOME/bin:$JAVA_HOME/bin' >> /etc/profile.d/javaenv.sh
     source /etc/profile.d/javaenv.sh
     writeInstallationSuccessfulMessage JAVA-JDK-18
 
     writeInstallationMessage JAVA-JDK-17
     wget https://download.oracle.com/java/17/archive/jdk-17_linux-x64_bin.tar.gz
     tar xf jdk-17_linux-x64_bin.tar.gz -C /usr/local/java
-
     update-alternatives --install "/usr/bin/java" "java" "/usr/local/java/jdk-17/bin/java" 2
     update-alternatives --install "/usr/bin/javac" "javac" "/usr/local/java/jdk-17/bin/javac" 2
     writeInstallationSuccessfulMessage JAVA-JDK-17
 
-
     writeInstallationMessage Spring-Boot-CLI
     wget https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.7.1/spring-boot-cli-2.7.1-bin.tar.gz
     tar xf spring-boot-cli-2.7.1-bin.tar.gz -C /opt
-    touch /etc/profile.d/springenv.env
-    echo ' ' >> /etc/profile.d/springenv.sh
-    echo 'export SPRING_HOME=/opt/spring-2.7.1/' >> /etc/profile.d/springenv.sh
-    echo 'export PATH=$PATH:$HOME/bin:$SPRING_HOME/bin' >> /etc/profile.d/springenv.sh
+    echo -e ' \n# Spring Boot CLI\nexport SPRING_HOME=/opt/spring-2.7.1/\nexport PATH=$PATH:$HOME/bin:$SPRING_HOME/bin' >>/etc/profile.d/springenv.sh
     source /etc/profile.d/springenv.sh
     writeInstallationSuccessfulMessage Spring-Boot-CLI
     ;;
@@ -236,19 +230,14 @@ for choice in $choices; do
     writeInstallationMessage Go
     wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz
     rm -rf /usr/local/go && tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
-    touch /etc/profile.d/goenv.sh
-    echo ' ' >>/etc/profile.d/goenv.sh
-    echo '# GoLang configuration ' >>/etc/profile.d/goenv.sh
-    echo 'export PATH="$PATH:/usr/local/go/bin"' >>/etc/profile.d/goenv.sh
-    echo 'export GOPATH="$HOME/go"' >>/etc/profile.d/goenv.sh
+    echo -e ' \n# GoLang configuration\nexport PATH="$PATH:/usr/local/go/bin"\nexport GOPATH="$HOME/go"' >>/etc/profile.d/goenv.sh
     source /etc/profile.d/goenv.sh
-    cd /tmp
     writeInstallationSuccessfulMessage Go
     ;;
   D4)
     writeInstallationMessage vscode
     rpm --import https://packages.microsoft.com/keys/microsoft.asc
-    sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo' sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list' rm -f packages.microsoft.gpg
+    dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
     dnf check-update
     dnf -y install code
     writeInstallationSuccessfulMessage vscode
@@ -260,16 +249,16 @@ for choice in $choices; do
     mv /opt/idea-IU-* /opt/idea-IU-${JETBRAINS_VERSION}
     ln -s /opt/idea-IU-${JETBRAINS_VERSION}/bin/idea.sh /usr/local/bin/idea
     echo "[Desktop Entry]
-          Version=1.0
-          Type=Application
-          Name=IntelliJ IDEA Ultimate Edition
-          Icon=/opt/idea-IU-${JETBRAINS_VERSION}/bin/idea.svg
-          Exec=/opt/idea-IU-${JETBRAINS_VERSION}/bin/idea.sh %f
-          Comment=Capable and Ergonomic IDE for JVM
-          Categories=Development;IDE;
-          Terminal=false
-          StartupWMClass=jetbrains-idea
-          StartupNotify=true;" >>/usr/share/applications/jetbrains-idea.desktop
+    Version=1.0
+    Type=Application
+    Name=IntelliJ IDEA Ultimate Edition
+    Icon=/opt/idea-IU-${JETBRAINS_VERSION}/bin/idea.svg
+    Exec=/opt/idea-IU-${JETBRAINS_VERSION}/bin/idea.sh %f
+    Comment=Capable and Ergonomic IDE for JVM
+    Categories=Development;IDE;
+    Terminal=false
+    StartupWMClass=jetbrains-idea
+    StartupNotify=true;" >>/usr/share/applications/jetbrains-idea.desktop
     writeInstallationSuccessfulMessage IntelliJ-IDEA
     ;;
   D6)
@@ -277,14 +266,13 @@ for choice in $choices; do
     wget https://download.jetbrains.com/go/goland-${JETBRAINS_VERSION}.tar.gz -O goland.tar.gz
     tar -xzf goland.tar.gz -C /opt
     ln -s /opt/GoLand-${JETBRAINS_VERSION}/bin/goland.sh /usr/local/bin/goland
-    echo "[Desktop Entry]
-          Version=1.0
-          Type=Application
-          Name=GoLand
-          Icon=/opt/GoLand-${JETBRAINS_VERSION}/bin/goland.png
-          Exec=/opt/GoLand-${JETBRAINS_VERSION}/bin/goland.sh
-          Terminal=false
-          Categories=Development;IDE;" >>/usr/share/applications/jetbrains-goland.desktop
+    echo -e "[Desktop Entry]
+    Version=1.0
+    Type=Application
+    Name=GoLand
+    Icon=/opt/GoLand-${JETBRAINS_VERSION}/bin/goland.png
+    Exec=/opt/GoLand-${JETBRAINS_VERSION}/bin/goland.sh
+    Terminal=false\nCategories=Development;IDE;" >>/usr/share/applications/jetbrains-goland.desktop
     writeInstallationSuccessfulMessage GoLand
     ;;
   D7)
@@ -323,7 +311,7 @@ for choice in $choices; do
     systemctl start docker
     docker run hello-world
     groupadd docker
-    usermod -a -G docker $USER 
+    usermod -a -G docker $USER
     writeInstallationSuccessfulMessage Docker
 
     writeInstallationMessage docker-compose
@@ -336,31 +324,35 @@ for choice in $choices; do
     writeInstallationMessage Maven
     wget https://dlcdn.apache.org/maven/maven-${MAVEN}/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
     tar -zxvf apache-maven-${MAVEN_VERSION}-bin.tar.gz -C /opt
-    cd /opt
-    ln -s apache-maven-${MAVEN_VERSION} maven
-    touch /etc/profile.d/maven.sh
+    ln -s /opt/apache-maven-${MAVEN_VERSION} /opt/maven
     echo ' ' >>/etc/profile.d/maven.sh
     echo '# Maven Configuration' >>/etc/profile.d/maven.sh
     echo 'JAVA_HOME=/usr/lib/jvm/default-java' >>/etc/profile.d/maven.sh
     echo 'export M2_HOME=/opt/maven' >>/etc/profile.d/maven.sh
     echo 'export PATH=${M2_HOME}/bin:${PATH}' >>/etc/profile.d/maven.sh
     source /etc/profile.d/maven.sh
-    cd /tmp
     writeInstallationSuccessfulMessage Maven
     ;;
   D10)
-    dnf -y install putty
+    writeInstallationMessage Gradle
+    wget https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip
+    mkdir /opt/gradle
+    unzip -d /opt/gradle gradle-${MAVEN_VERSION}-bin.zip
+    echo ' ' >>/etc/profile.d/gradle.sh
+    echo 'export PATH=$PATH:/opt/gradle/gradle-${GRADLE_VERSION}/bin' >>/etc/profile.d/gradle.sh
+    writeInstallationSuccessfulMessage Gradle
     ;;
   D11)
-    dnf -y install vim
+    writeInstallationMessage PuTTY
+    dnf -y install putty
+    writeInstallationSuccessfulMessage PuTTY
     ;;
   D12)
-    snap install pycharm-community --classic
+    writeInstallationMessage Vim
+    dnf -y install vim
+    writeInstallationSuccessfulMessage Vim
     ;;
   D13)
-    snap install robo3t-snap
-    ;;
-  D14)
     writeInstallationMessage DataGrip
     wget https://download.jetbrains.com/datagrip/datagrip-${JETBRAINS_VERSION}.tar.gz
     tar -xzf datagrip-${JETBRAINS_VERSION}.tar.gz -C /opt
@@ -375,7 +367,7 @@ for choice in $choices; do
           Categories=Development;IDE;" >>/usr/share/applications/jetbrains-datagrip.desktop
     writeInstallationSuccessfulMessage DataGrip
     ;;
-  D15)
+  D14)
     wget -O mongosh.rpm https://downloads.mongodb.com/compass/mongodb-mongosh-1.2.2.el8.x86_64.rpm
     dpkg -i ./mongosh.rpm
 
@@ -384,46 +376,44 @@ for choice in $choices; do
     ;;
 
   E1)
+    writeInstallationMessage Gnome-Twek-Tool
     dnf install -y gnome-tweak-tool
+    writeInstallationSuccessfulMessage Gnome-Twek-Tool
     ;;
   E2)
+    writeInstallationMessage Gnome-Extensions
     dnf install -y gnome-extensions-app
     dnf install -y gnome-shell-extension-appindicator
+    writeInstallationSuccessfulMessage Gnome-Extensions
     ;;
 
   F1)
+    writeInstallationMessage Dropbox
     wget -O dropbox.rpm https://www.dropbox.com/download?dl=packages/fedora/nautilus-dropbox-2020.03.04-1.fedora.x86_64.rpm
     dnf install -y ./dropbox.rpm
+    writeInstallationSuccessfulMessage Dropbox
+    echo -e "[Dropbox]\nname=Dropbox Repository\baseurl=http://linux.dropbox.com/fedora/35/\n    gpgkey=https://linux.dropbox.com/fedora/rpm-public-key.asc" >/etc/yum.repos.d/dropbox.repo
     ;;
   F2)
+    writeInstallationMessage KeePassXC
     dnf install -y keepassxc
+    writeInstallationSuccessfulMessage KeePassXC
     ;;
   F3)
-    # TODO: Write command to install the VirtualBox
+    writeInstallationMessage Terminator
+    dnf -y install terminator
+    writeInstallationSuccessfulMessage Terminator
     ;;
   F4)
-    dnf -y install terminator
-    ;;
-  F5)
-    # TODO: Fix configuration
-    dnf -y instal powerline fonts-powerline
-    ;;
-  F6)
-    dnf -y install htop
-    ;;
-  F7)
-    git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-    sh ~/.vim_runtime/install_awesome_vimrc.sh
-    ;;
-  F8)
-    dnf install ulauncher
+    writeInstallationMessage Boxes
+    dnf install gnome-boxes
+    writeInstallationSuccessfulMessage Boxes
     ;;
 
   G1)
     dnf -y install gimp
     ;;
   G2)
-    cd /tmp/
     wget -O droidcam_latest.zip https://files.dev47apps.net/linux/droidcam_1.8.1.zip
     unzip droidcam_latest.zip -d droidcam
     cd droidcam && ./install-client
@@ -432,21 +422,25 @@ for choice in $choices; do
     dnf -y install libappindicator-gtk3
     ;;
 
-  *)
+  *) ;;
   esac
 done
 
+printf "${GREEN}"
 cat <<EOL
----------------------------------------------------------------
+========================================================================
 Congratulations, everything you wanted to install is installed!
----------------------------------------------------------------
+========================================================================
 EOL
+printf "${ENDCOLOR}"
 
 cat <<EOL
 
 EOL
 
+printf "${BLUE}"
 read -p "Are you going to reboot this machine for stability? (y/n): " -n 1 answer
+printf "${ENDCOLOR}"
 if [[ $answer =~ ^[Yy]$ ]]; then
   reboot
 fi
