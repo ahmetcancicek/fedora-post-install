fe25e5bf10e95c93ea7ab518c24a05bd8eb8b63a
Ahmet Can Cicek <ahmetcan@ahmetcancicek.com>
2022-07-22 12:00:53 +0300

    Fix delete temp folder

--- install-fonts.sh
+++ install-fonts.sh
@@ -48,32 +48,32 @@ dnf install -y google-droid-sans-mono-fonts
 dnf install -y dejavu-sans-mono-fonts
 
 # Hack v3
+rm -rf /usr/share/fonts/hack hack-v3 hack
 wget -O hack-v3.zip https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip
 unzip hack-v3.zip -d hack-v3
 mkdir hack
 mv hack-v3/ttf/* hack
-rm -rf /usr/share/fonts/hack
 mv hack /usr/share/fonts/hack
 fc-cache -f .
 cd -
 
 
 # Ubuntu Font Family
+rm -rf /usr/share/fonts/ubuntu-font-family ubuntu-font-family-0.83
 wget -O ubuntu-font-family.zip https://assets.ubuntu.com/v1/0cef8205-ubuntu-font-family-0.83.zip
 unzip ubuntu-font-family.zip
 mv ubuntu-font-family-0.83 ubuntu-font-family
-rm -rf /usr/share/fonts/ubuntu-font-family
 mv ubuntu-font-family /usr/share/fonts/ubuntu-font-family
 fc-cache -f .
 cd -
 
 
 # JetBrains Mono Font Family
+rm -rf /usr/share/fonts/jetbrains-mono JetBrainsMono-2.242 jetbrains-mono
 wget https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip
 unzip JetBrainsMono-2.242 -d JetBrainsMono-2.242
 mkdir jetbrains-mono
 mv JetBrainsMono-2.242/fonts/ttf/** jetbrains-mono
-rm -rf /usr/share/fonts/jetbrains-mono
 mv jetbrains-mono /usr/share/fonts/jetbrains-mono
 fc-cache -f .
 cd -
